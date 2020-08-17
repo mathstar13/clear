315c76aec5ec6c0b137235dd1344b668e7eeaf8d
Alex D <68231126+mathstar13@users.noreply.github.com>
2020-08-17 18:10:32 -0400

    Add files via upload

---
+++ cleargreatusername.py
@@ -0,0 +1,11 @@
+from os import system, name
+def clear():
+	if name == 'nt':
+		_ = system('cls')
+	else:
+		_ = system('clear')
+def cls():
+	if name == 'nt':
+		_ = system('cls')
+	else:
+		_ = system('clear')
